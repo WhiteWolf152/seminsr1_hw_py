39145c190de9f5b9927bcc0180afc42cadc0c1b3
PletenkovAndrey <and1257272@mail.ru>
2022-07-28 17:24:34 +0300

    add_hw_task2

---
+++ task2.py
@@ -0,0 +1,15 @@
+# 2. Напишите программу для проверки истинности
+#  утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для 
+# всех значений предикат.
+
+print('x y z - Первое выражение Второе выражение')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            result1 = not x and not y and not z
+            result2 = not(x or y or z)
+            print(x, y, z, "       ", result1, "         ", result2)
+if result1 == result2:
+    print("Равенство истенно")
+else:
+    print("Равенство ложно")
